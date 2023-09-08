424cf00886194c0ef7f0def0de670f1aa03bf7f5
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-09-07 21:26:09 -0300

    feat: add assets caching with vite-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,80 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      includeAssets: ["assets/*", "components/Icons.js"],
+      manifest: {
+        name: "nós.short - O melhor encurtador de links",
+        description: "Encurte, salve e compartilhe links em poucos cliques!",
+        short_name: "nós.short",
+        icons: [
+          {
+            src: "/favicon-72x72.png",
+            type: "image/png",
+            sizes: "72x72",
+            purpose: "any maskable",
+          },
+          {
+            src: "/favicon-96x96.png",
+            type: "image/png",
+            sizes: "96x96",
+            purpose: "any maskable",
+          },
+          {
+            src: "/favicon-128x128.png",
+            type: "image/png",
+            sizes: "128x128",
+            purpose: "any maskable",
+          },
+          {
+            src: "/favicon-144x144.png",
+            type: "image/png",
+            sizes: "144x144",
+            purpose: "any maskable",
+          },
+          {
+            src: "/favicon-152x152.png",
+            type: "image/png",
+            sizes: "152x152",
+            purpose: "any maskable",
+          },
+          {
+            src: "/favicon-192x192.png",
+            type: "image/png",
+            sizes: "192x192",
+            purpose: "any maskable",
+          },
+          {
+            src: "/favicon-384x384.png",
+            type: "image/png",
+            sizes: "384x384",
+            purpose: "any maskable",
+          },
+          {
+            src: "/favicon-512x512.png",
+            type: "image/png",
+            sizes: "512x512",
+            purpose: "any maskable",
+          },
+        ],
+        scope: "/",
+        start_url: "/?source=pwa",
+        display: "standalone",
+        theme_color: "#fcd34d",
+        background_color: "#fafafa",
+      },
+      devOptions: {
+        enabled: !process.env.production,
+      },
+    }),
+  ],
+});
